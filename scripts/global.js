252b416fe39c169e7e27fa87f8558c2758445e7f
albertbobo <einstein1995@163.com>
2021-05-10 02:20:23 +0800

    refactor to banner-wrapper

--- scripts/global.js
+++ scripts/global.js
@@ -29,7 +29,7 @@ function highlightPage() {
     }
 }
 
-// pages/works 点击子导航显示相应的内容
+// pages/works 点击侧栏显示相应的内容
 function showSection(id) {
     var sections = document.getElementsByTagName("section");
     for (var i = 0; i < sections.length; i++) {
@@ -40,18 +40,30 @@ function showSection(id) {
         }
     }
 }
+
 function prepareInternalnav() {
     if (!document.getElementsByTagName) return false;
     if (!document.getElementById) return false;
-    var subnavs = document.getElementsByClassName("subnav");
-    if (subnavs.length === 0) return false;
-    var subnav = subnavs[0];
-    var links = subnav.getElementsByTagName("a");
+    var sidebars = document.getElementsByClassName("sidebar");
+    var selectedStudents = document.getElementsByClassName("selected-students")[0];
+    var sidebarTeachers = document.getElementsByClassName("sidebar-teachers")[0];
+    if (sidebars.length === 0) return false;
+    var sidebar = sidebars[0];
+    var links = sidebar.getElementsByTagName("a");
 
     for (var i = 0; i < links.length; i++) {
         var sectionId = links[i].getAttribute("href").split("#")[1];
         links[i].destination = sectionId;    // 解决sectionId是局部变量的问题，为每个链接创建一个自定义属性destination
         links[i].onclick = function () {
+            // 切换按钮样式
+            if (this.destination === "students-works") {
+                selectedStudents.className = "selected-students";
+                sidebarTeachers.className = "sidebar-teachers";
+            } else if (this.destination === "teachers-works") {
+                sidebarTeachers.className = "selected-teachers";
+                selectedStudents.className = "sidebar-students";
+            }
+
             showSection(this.destination);
             return false;
         }
